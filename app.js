47a26109dedeb79cb95f8226c274623e659d11e6
Student <jeff.kandel@gmail.com>
2017-04-27 11:00:46 -0400

    feat: add express server

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+//built-in modules
+const path = require('path');
+
+//npm modules
+// npm i --save express morgan
+// (from other parts)
+const express = require('express');
+const morgan = require('morgan');
+
+//instantiate the express app
+const app = express();
+
+// logging middleware
+app.use(morgan('dev'));
+
+// static and dynamic routing
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3000, function(){
+  console.log('listening on port 3000');
+});
