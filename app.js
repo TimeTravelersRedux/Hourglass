04bf3cf3758c239959fb1dfd467c9bcfe6b2f471
Student <jeff.kandel@gmail.com>
2017-04-27 12:52:37 -0400

    feat: install socket.io and add start/js/client.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 'use strict';
-//built-in modules
+// built-in modules
 const path = require('path');
 
-//npm modules
+// npm modules
 const express = require('express');
 const morgan = require('morgan');
 
-//instantiate the express app
+// instantiate the express app
 const app = express();
+
+// instantiate web sockets
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // logging middleware
 app.use(morgan('dev'));
 
-// static and dynamic routing
-app.use(express.static(path.join(__dirname, '/start')));
+// set up static routes
+app.use('/images', express.static(__dirname + '/start/images'))
+app.use('/js', express.static(__dirname + '/start/js'))
+app.use('/data', express.static(__dirname + '/start/data'))
 
-app.listen(3000, function(){
-  console.log('listening on port 3000');
-});
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/start/index.html')
+})
+server.listen(3000, function() {
+  console.log('listening on port 3000')
+})
