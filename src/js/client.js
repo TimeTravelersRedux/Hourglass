8206064455fdd7e3594de7d82d85fa3af0382929
Student <jeff.kandel@gmail.com>
2017-04-28 11:55:08 -0400

    chore: rename old files before removal

--- src/js/client.js
+++ src/js/client.js
@@ -1,20 +1,20 @@
-var Client = {};
-Client.socket = io.connect();
+var Client = {}
+Client.socket = io.connect()
 
-Client.askNewPlayer = function(){
-  Client.socket.emit('newplayer');
-};
+Client.askNewPlayer = function() {
+  Client.socket.emit('newplayer')
+}
 
-Client.socket.on('newplayer',function(data){
-    PlayState.addNewPlayer(data.id,data.x,data.y);
-});
+Client.socket.on('newplayer', function(data) {
+  PlayState.addNewPlayer(data.id, data.x, data.y)
+})
 
-Client.socket.on('allplayers',function(data){
-    for(var i = 0; i < data.length; i++){
-        PlayState.addNewPlayer(data[i].id,data[i].x,data[i].y);
-    }
-});
+Client.socket.on('allplayers', function(data) {
+  for (var i = 0; i < data.length; i++) {
+    PlayState.addNewPlayer(data[i].id, data[i].x, data[i].y)
+  }
+})
 
-Client.socket.on('remove',function(id){
-    PlayState.removePlayer(id);
-});
+Client.socket.on('remove', function(id) {
+  PlayState.removePlayer(id)
+})
